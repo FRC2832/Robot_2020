6a75323cc3049c580fa9b95c1dbbbe517890f867
msnider <msnider@livoniawarriors.org>
2020-01-11 14:54:29 -0500

    Added ingestor
    File for ingestor has been made and code for the ingestor has been added tothe holotable.

--- src/main/java/frc/robot/HoloTable.java
+++ src/main/java/frc/robot/HoloTable.java
@@ -9,27 +9,39 @@ import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
 import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.Solenoid;
+import edu.wpi.first.wpilibj.SpeedControllerGroup;
+import edu.wpi.first.wpilibj.XboxController;
 
 public final class HoloTable {
 
     private static HoloTable instance = null;
     // public static HashMap map = new HashMap<String, Object>(); For Hashmap
+    private static WPI_TalonSRX intakeLeft;
+    private static WPI_TalonSRX intakeRight;
     private static WPI_TalonSRX driveTurn;
     private static CANSparkMax driveRightFront;
     private static PigeonIMU gyro;
     private static Solenoid singleSolenoid;
     private static DoubleSolenoid turnSolenoid;
+    private static SpeedControllerGroup intake;
+    private static XboxController gamepad1;
+    
     // private static Insert Camera Here;
     // private static Insert Color Sensor Here;
 
     private HoloTable() {
 
         // map.put("driveTurn", ); For Hashmap
+        intakeLeft = new WPI_TalonSRX(0);
+        intakeLeft.setInverted(true);
+        intakeRight = new WPI_TalonSRX(0);
         driveTurn = new WPI_TalonSRX(0);
         driveRightFront = new CANSparkMax(0, MotorType.kBrushless);
         gyro = new PigeonIMU(0);
         singleSolenoid = new Solenoid(0);
         turnSolenoid = new DoubleSolenoid(1, 2);
+        intake = new SpeedControllerGroup(intakeLeft, intakeRight);
+        gamepad1 = new XboxController(1);
 
     }
 
@@ -61,6 +73,12 @@ public final class HoloTable {
 
     public DoubleSolenoid getTurnSolenoid() {
         return turnSolenoid;
+    }
+    public SpeedControllerGroup getIntake(){
+        return intake;
+    }
+    public XboxController getGamepad1() {
+        return gamepad1;
     }
 
 }
