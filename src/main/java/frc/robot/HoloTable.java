b43d02558a062c670d9be41f3216cf830fc2bfa7
msnider <msnider@livoniawarriors.org>
2020-01-10 20:12:08 -0500

    Added Getter
    I added getters and a singleton in clase de los HoloTable.

--- src/main/java/frc/robot/HoloTable.java
+++ src/main/java/frc/robot/HoloTable.java
@@ -10,32 +10,57 @@ import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.Solenoid;
 
-
 public final class HoloTable {
 
-  //  private static HoloTable instance = null;                   For Hashmap
-  //  public static HashMap map = new HashMap<String, Object>();  For Hashmap
-    public static WPI_TalonSRX driveTurn;
-    public static CANSparkMax driveRightFront;
-    public static PigeonIMU gyro;
-    public static Solenoid singleSolenoid;
-    public static DoubleSolenoid turnSolenoid;
-    //public static Insert Camera Here;
-    //public static Insert Color Sensor Here;
-  
+    private static HoloTable instance = null;
+    // public static HashMap map = new HashMap<String, Object>(); For Hashmap
+    private static WPI_TalonSRX driveTurn;
+    private static CANSparkMax driveRightFront;
+    private static PigeonIMU gyro;
+    private static Solenoid singleSolenoid;
+    private static DoubleSolenoid turnSolenoid;
+    // private static Insert Camera Here;
+    // private static Insert Color Sensor Here;
 
-      private HoloTable() {
+    private HoloTable() {
 
-        //map.put("driveTurn", );                                 For Hashmap
+        // map.put("driveTurn", ); For Hashmap
         driveTurn = new WPI_TalonSRX(0);
         driveRightFront = new CANSparkMax(0, MotorType.kBrushless);
         gyro = new PigeonIMU(0);
         singleSolenoid = new Solenoid(0);
         turnSolenoid = new DoubleSolenoid(1, 2);
-        
 
     }
 
-    
-        
+    public static HoloTable getInstance() {
+
+        if (instance == null) {
+            instance = new HoloTable();
+        }
+
+        return instance;
+
+    }
+
+    public WPI_TalonSRX getDriveTurn() {
+        return driveTurn;
+    }
+
+    public CANSparkMax getDriveRightFront() {
+        return driveRightFront;
+    }
+
+    public PigeonIMU getGyro() {
+        return gyro;
+    }
+
+    public Solenoid getSingleSoleniod() {
+        return singleSolenoid;
+    }
+
+    public DoubleSolenoid getTurnSolenoid() {
+        return turnSolenoid;
+    }
+
 }
