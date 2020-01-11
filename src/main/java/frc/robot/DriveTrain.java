970edbaf0f7c16c2d6b8fa0ba2d49bd6ab5f5179
Spencer Russell <xxtoastrulezxx@gmail.com>
2020-01-11 01:21:20 -0500

    Update DriveTrain.java

--- src/main/java/frc/robot/DriveTrain.java
+++ src/main/java/frc/robot/DriveTrain.java
@@ -5,20 +5,37 @@
 /* the project.                                                               */
 /*----------------------------------------------------------------------------*/
 
-package frc.robot;
 
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-public class DriveTrain extends SubsystemBase {
+
+
+public class DriveTrain extends SubsystemBase{
+  private WPI_TalonSRX driveTurn;
+  private CANSparkMax leftFront; 
+	private CANSparkMax rightFront;
+	private CANSparkMax leftRear;
+	private CANSparkMax rightRear;
+  private DifferentialDrive differentialDrive;
   /**
    * Creates a new DriveTrain.
    */
   public DriveTrain() {
-
+    driveTurn = HoloTable.getDriveTurn();
+    leftFront = HoloTable.getDriveLeftFront();
+    rightFront = HoloTable.getDriveRightFront();
+    leftRear= HoloTable.getDriveLeftRear();
+    rightRear = HoloTable.getDriveRightRear();
   }
 
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
   }
+
+  public void setDifferentialDriveCommand(double xSpeed, double zRotation) {
+    //WRITE STUFF HERE ONCE YOU HAVE INPUTS
+    differentialDrive.arcadeDrive(xSpeed, zRotation, true);
+}
 }
