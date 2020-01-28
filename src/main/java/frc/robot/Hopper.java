75f2c1280ee5532724c4bb27c979201b67289352
JC-Sidaway <54559313+JC-Sidaway@users.noreply.github.com>
2020-01-27 19:30:43 -0500

    Formatting
    just formatting

--- src/main/java/frc/robot/Hopper.java
+++ src/main/java/frc/robot/Hopper.java
@@ -4,37 +4,32 @@ import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 
 import edu.wpi.first.wpilibj.XboxController;
 
-public final class Hopper{
+public final class Hopper {
 
     HoloTable table = HoloTable.getInstance();
 
     WPI_TalonSRX hopper;
     XboxController gamepad1;
 
-    private Hopper(){
-        
-    hopper = table.getHopper();
-    gamepad1 = table.getGamepad1();
+    private Hopper() {
 
+        hopper = table.getHopper();
+        gamepad1 = table.getGamepad1();
 
     }
-   
-    public void RunMotors(){
+
+    public void RunMotors() {
         if (gamepad1.getAButtonPressed()) {
             hopper.set(.5);
-        }
-        else if (gamepad1.getAButtonReleased()) {
+        } else if (gamepad1.getAButtonReleased()) {
             hopper.set(0);
         }
         if (gamepad1.getStartButtonPressed()) {
             hopper.set(-.5);
-        }
-        else if (gamepad1.getStartButtonReleased()){
+        } else if (gamepad1.getStartButtonReleased()) {
             hopper.set(0);
         }
-        
-        
+
     }
-    
 
 }
