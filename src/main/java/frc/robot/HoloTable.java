b5d3ee1f5065fafa1448957cd3eee907e480b9c0
msnider <msnider@livoniawarriors.org>
2020-02-07 20:39:54 -0500

    Update HoloTable.java

--- src/main/java/frc/robot/HoloTable.java
+++ src/main/java/frc/robot/HoloTable.java
@@ -9,7 +9,6 @@ import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.Joystick;
-import edu.wpi.first.wpilibj.Solenoid;
 import edu.wpi.first.wpilibj.XboxController;
 
 public final class HoloTable {
