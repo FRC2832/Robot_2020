e65659d90e2a130a6dad72dbea7fa792d0e524f2
msnider <msnider@livoniawarriors.org>
2020-02-14 19:19:19 -0500

    Motors and Gyro added to Option4
    All drive motors added, along with created speedcontroller groups, and a gyro.

---
+++ src/main/java/frc/robot/commands/auton/Option4.java
@@ -0,0 +1,48 @@
+package frc.robot.commands.auton;
+
+import com.ctre.phoenix.sensors.PigeonIMU;
+import com.revrobotics.CANSparkMax;
+
+import edu.wpi.first.wpilibj.SpeedControllerGroup;
+import edu.wpi.first.wpilibj.command.Command;
+import frc.robot.HoloTable;
+
+public class Option4 extends Command {
+    HoloTable holo = HoloTable.getInstance();
+    PigeonIMU gyro = holo.getGyro();
+    CANSparkMax leftFront = holo.getDriveLeftFront();
+    CANSparkMax rightFront = holo.getDriveRightFront();
+    CANSparkMax leftRear = holo.getDriveLeftRear();
+    CANSparkMax rightRear = holo.getDriveRightRear();
+    SpeedControllerGroup leftMotors= new SpeedControllerGroup(leftFront, leftRear);
+    SpeedControllerGroup rightMotors= new SpeedControllerGroup(rightFront, rightRear);
+
+    @Override
+    protected void initialize() {
+        leftMotors.setInverted(true);
+        rightMotors.setInverted(true);
+        gyro.setYaw(0);
+        // TODO Auto-generated method stub
+        super.initialize();
+    }
+
+    @Override
+    protected void execute() {
+        gyro.setYaw(0);
+        // TODO Auto-generated method stub
+        super.execute();
+    }
+
+    @Override
+    protected void end() {
+        // TODO Auto-generated method stub
+        super.end();
+    }
+
+    @Override
+    public boolean isFinished() {
+        // TODO Auto-generated method stub
+        return true;
+    }
+
+}
