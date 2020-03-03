9fcc9de5b474b45be5a0a72574738f107785bdcd
jcsidawy <46980470+jcsidawy@users.noreply.github.com>
2020-03-02 20:23:47 -0500

    Fixed indent
    got ride of a indent (line 62 drive train)

--- src/main/java/frc/robot/DriveTrain.java
+++ src/main/java/frc/robot/DriveTrain.java
@@ -1,52 +1,77 @@
 package frc.robot;
 
 /*----------------------------------------------------------------------------*/
-
 /* Copyright (c) 2019 FIRST. All Rights Reserved.                             */
 /* Open Source Software - may be modified and shared by FRC teams. The code   */
 /* must be accompanied by the FIRST BSD license file in the root directory of */
 /* the project.                                                               */
 /*----------------------------------------------------------------------------*/
 
-import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+import com.ctre.phoenix.sensors.PigeonIMU;
 import com.revrobotics.CANSparkMax;
+
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.SpeedControllerGroup;
+import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
 public class DriveTrain extends Subsystem {
   private HoloTable holo = HoloTable.getInstance();
+  private WPI_TalonSRX driveTurn;
   private CANSparkMax leftFront;
   private CANSparkMax rightFront;
   private CANSparkMax leftRear;
   private CANSparkMax rightRear;
   private DifferentialDrive differentialDrive;
-  //private DoubleSolenoid turnSolenoid;
+  private PigeonIMU gyro;
+  private XboxController controller;
+  private DoubleSolenoid turnSolenoid;
   private SpeedControllerGroup leftMotors;
   private SpeedControllerGroup rightMotors;
   private Joystick joystickLeft;
   private Joystick joystickRight;
   private double driveCoeff;
+  
 
   /**
    * Creates a new DriveTrain.
    */
   public DriveTrain() {
+    driveTurn = holo.getDriveTurn();
     leftFront = holo.getDriveLeftFront();
     rightFront = holo.getDriveRightFront();
     leftRear = holo.getDriveLeftRear();
     rightRear = holo.getDriveRightRear();
+    gyro = holo.getGyro();
+    controller = holo.getController();
     joystickLeft = holo.getJoystickLeft();
     joystickRight = holo.getJoystickRight();
-    //turnSolenoid = holo.getTurnSolenoid();
     leftMotors = new SpeedControllerGroup(leftFront, leftRear);
     leftMotors.setInverted(true);
     rightMotors = new SpeedControllerGroup(rightFront, rightRear);
     rightMotors.setInverted(true);
     differentialDrive = new DifferentialDrive(leftMotors, rightMotors);
     driveCoeff = 1;
+    openLoopRampRate(0.8);
+    setSmartCurrent(70);
   }
 
+  public void setSmartCurrent(int limit) {
+    leftFront.setSmartCurrentLimit(limit);
+    leftRear.setSmartCurrentLimit(limit);
+    rightRear.setSmartCurrentLimit(limit);
+    rightFront.setSmartCurrentLimit(limit);
+  }
+
+  public void openLoopRampRate(double rate) {
+    leftFront.setOpenLoopRampRate(rate);
+    leftRear.setOpenLoopRampRate(rate);
+    rightRear.setOpenLoopRampRate(rate);
+    rightFront.setOpenLoopRampRate(rate);
+  }
 
   @Override
   public void periodic() {
@@ -55,21 +80,21 @@ public class DriveTrain extends Subsystem {
   }
 
   public void driveTank() {
-    if(joystickRight.getRawButton(2))
+    if (joystickRight.getRawButton(2))
       driveCoeff = .3;
     else
       driveCoeff = 1;
-      differentialDrive.tankDrive(driveCoeff * Math.pow(joystickLeft.getY(), 3 ) * 0.5, driveCoeff * Math.pow(joystickRight.getY(), 3 ) * 0.5, false);
-    } 
+    differentialDrive.tankDrive(driveCoeff * Math.pow(joystickLeft.getY(), 3) * 0.5, driveCoeff * Math.pow(joystickRight.getY(), 3) * 0.5, false);
+  }
   /*public void driveArcade() {
     differentialDrive.arcadeDrive(controller.getRawAxis(0), controller.getRawAxis(4), true);  Not Being Used
   }*/
 
   public void extendDriveTurn(){
-    //turnSolenoid.set(DoubleSolenoid.Value.kForward);
+    turnSolenoid.set(DoubleSolenoid.Value.kForward);
   }
   public void retractDriveTurn(){
-    //turnSolenoid.set(DoubleSolenoid.Value.kReverse);
+    turnSolenoid.set(DoubleSolenoid.Value.kReverse);
   }
 
   @Override
